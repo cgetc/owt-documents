bcbce04dea2c33ec018be40bcdcadf29512b4485
cgetc <shigetoshi.komatsu@mixi.co.jp>
2019-07-19 10:08:19 +0900

    add generated documents

---
+++ owt-server/cpHtml/navtreedata.js
@@ -0,0 +1,14 @@
+var NAVTREE =
+[
+  [ "Intel® Collaboration Suite for WebRTC", "index.html", [
+    [ "Client-Portal Communication Protocol", "index.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ owt-server/restHtml/navtreedata.js
@@ -0,0 +1,79 @@
+var NAVTREE =
+[
+  [ "Intel® Collaboration Suite for WebRTC", "index.html", [
+    [ "Open WebRTC Toolkit(OWT) Server Management REST API Guide", "index.html", [
+      [ "1 Introduction", "index.html#RESTAPIsection1", null ],
+      [ "2 Definitions", "index.html#RESTAPIsection2", null ],
+      [ "3 Making a Call", "index.html#RESTAPIsection3", null ],
+      [ "4 Authentication and Authorization", "index.html#RESTAPIsection4", null ],
+      [ "5 Management Resource", "index.html#RESTAPIsection5", [
+        [ "5.1 Rooms", "index.html#RESTAPIsection5_1", [
+          [ "Create Room", "index.html#RESTAPIsection5_1_1", null ],
+          [ "List Rooms", "index.html#RESTAPIsection5_1_2", null ],
+          [ "Get Room", "index.html#RESTAPIsection5_1_3", null ],
+          [ "Delete Room", "index.html#RESTAPIsection5_1_4", null ],
+          [ "Update Room", "index.html#RESTAPIsection5_1_5", null ]
+        ] ],
+        [ "5.2 Participants", "index.html#RESTAPIsection5_2", [
+          [ "List Participants", "index.html#RESTAPIsection5_2_1", null ],
+          [ "Get Participant", "index.html#RESTAPIsection5_2_2", null ],
+          [ "Update Participant", "index.html#RESTAPIsection5_2_3", null ],
+          [ "Drop Participant", "index.html#RESTAPIsection5_2_4", null ]
+        ] ],
+        [ "5.3 Streams", "index.html#RESTAPIsection5_3", [
+          [ "List Streams", "index.html#RESTAPIsection5_3_1", null ],
+          [ "Get Stream", "index.html#RESTAPIsection5_3_2", null ],
+          [ "Update Stream", "index.html#RESTAPIsection5_3_3", null ],
+          [ "Drop Stream", "index.html#RESTAPIsection5_3_4", null ]
+        ] ],
+        [ "5.4 Streaming-ins", "index.html#RESTAPIsection5_4", [
+          [ "Start Streaming-in", "index.html#RESTAPIsection5_4_1", null ],
+          [ "Stop Streaming-in", "index.html#RESTAPIsection5_4_2", null ]
+        ] ],
+        [ "5.5 Streaming-outs", "index.html#RESTAPIsection5_5", [
+          [ "List Streaming-outs", "index.html#RESTAPIsection5_5_1", null ],
+          [ "Start Streaming-outs", "index.html#RESTAPIsection5_5_2", null ],
+          [ "Update Streaming-out", "index.html#RESTAPIsection5_5_3", null ],
+          [ "Stop Streaming-out", "index.html#RESTAPIsection5_5_4", null ]
+        ] ],
+        [ "5.6 Recordings", "index.html#RESTAPIsection5_6", [
+          [ "List Recordings", "index.html#RESTAPIsection5_6_1", null ],
+          [ "Start Recordings", "index.html#RESTAPIsection5_6_2", null ],
+          [ "Update Recording", "index.html#RESTAPIsection5_6_3", null ],
+          [ "Stop Recording", "index.html#RESTAPIsection5_6_4", null ]
+        ] ],
+        [ "5.7 Sipcalls", "index.html#RESTAPIsection5_7", [
+          [ "Get SIP Call", "index.html#RESTAPIsection5_7_1", null ],
+          [ "New SIP Call", "index.html#RESTAPIsection5_7_2", null ],
+          [ "Update SIP call", "index.html#RESTAPIsection5_7_3", null ],
+          [ "Delete SIP Call", "index.html#RESTAPIsection5_7_4", null ]
+        ] ],
+        [ "5.8 Analytics", "index.html#RESTAPIsection5_8", [
+          [ "List Analytics", "index.html#RESTAPIsection5_8_1", null ],
+          [ "Start Analytics", "index.html#RESTAPIsection5_8_2", null ],
+          [ "Stop Analytics", "index.html#RESTAPIsection5_8_3", null ]
+        ] ],
+        [ "5.9 Token", "index.html#RESTAPIsection5_9", [
+          [ "Create Token", "index.html#RESTAPIsection5_9_1", null ]
+        ] ],
+        [ "5.10 Service", "index.html#RESTAPIsection5_10", [
+          [ "List Services", "index.html#RESTAPIsection5_10_1", null ],
+          [ "Create Service", "index.html#RESTAPIsection5_10_2", null ],
+          [ "Get Service", "index.html#RESTAPIsection5_10_3", null ]
+        ] ]
+      ] ],
+      [ "6 Media Constraints", "index.html#RESTAPIsection6", [
+        [ "6.1 Audio", "index.html#RESTAPIsection6_1", null ],
+        [ "6.2 Video", "index.html#RESTAPIsection6_2", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
